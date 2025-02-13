66fa069c999e314d1c2f57cebc5946e82da0a153
yavuzkutuk <yavuz@kutuk.fr>
2025-02-13 10:43:50 +0100

    init

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,14 @@
+import express from "express";
+import cors from "cors";
+const app = express();
 // Load environment variables from .env file
 import "dotenv/config";
+
+if (process.env.CLIENT_URL != null) {
+	app.use(cors({ origin: [process.env.CLIENT_URL] }));
+} else {
+	app.use(cors());
+}
 
 // Check database connection
 // Note: This is optional and can be removed if the database connection
@@ -7,7 +16,19 @@ import "dotenv/config";
 /* import "../database/checkConnection"; */
 
 // Import the Express application from ./app
-import app from "./app";
+
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.text());
+app.use(express.raw());
+
+/* ************************************************************************* */
+
+// Import the API router
+import router from "./router";
+
+// Mount the API router under the "/api" endpoint
+app.use(router);
 
 // Get the port from the environment variables
 const port = process.env.APP_PORT;
